d8e74731b66e1598f2df8e22fb5796f830776cfc
Mateus <maatmds04@gmail.com>
2023-04-24 11:31:30 -0300

    Herança com a estratégia Join foi implementada com sucesso

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,9 +1,28 @@
 package br.com.fiap;
 
+import br.com.fiap.pessoa.model.PF;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("maria-db");
+        EntityManager entityManager = factory.createEntityManager();
+
+        PF mateus = new PF();
+        mateus.setCPF("123123123");
+        mateus.setRG("1231232");
+        mateus.setNome("Mateus Mendes dos Santos");
+        mateus.setNascimento(LocalDate.of(2004, 7, 24));
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(mateus);
+        entityManager.getTransaction().commit();
 
     }
 
--- src/main/java/br/com/fiap/pessoa/model/Pessoa.java
+++ src/main/java/br/com/fiap/pessoa/model/Pessoa.java
@@ -1,20 +1,35 @@
 package br.com.fiap.pessoa.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
-
+@Data
+@Entity
+@Table(name = "TB_PESSOA")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "TP_PESSOA")
+@NoArgsConstructor
+@AllArgsConstructor
 public abstract class Pessoa {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_PESSOA")
+    @SequenceGenerator(
+            name = "SQ_PESSOA",
+            sequenceName = "SQ_PESSOA"
+    )
 
-    private
-    Long id;
+    @Column(name = "ID_PESSOA")
+    private Long id;
 
+    @Column(name = "NM_PESSOA")
+    private String nome;
 
-    private
-    String nome;
-
-
-    private
-    LocalDate nascimento;
+    @Column(name = "DT_NASC_PESSOA")
+    private LocalDate nascimento;
 
 }
